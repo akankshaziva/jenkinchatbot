28c42add2fcc67986dc291b0a317e61fee15f16d
Akanksha Kadvekar <91193956+akankshaziva@users.noreply.github.com>
2022-02-09 17:12:54 +0530

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -3,25 +3,31 @@ from json import dumps
 from httplib2 import Http
 
 
-def main():
-    """Hangouts Chat incoming webhook quickstart."""
-    url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
-    bot_message = {
-        'text' : "Job_name - %d\n %s" %({JOB_NAME},status)
 
-    #message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+def main():
+    """Google Chat incoming webhook quickstart."""
+    url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
+    bot_message = {'text' :'Hello from a Python script!'}
+        
+        
+    
+        
+
+    message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
     http_obj = Http()
 
     response = http_obj.request(
         uri=url,
         method='POST',
-        #headers=message_headers,
+        headers=message_headers,
         body=dumps(bot_message),
+        
     )
 
     print(response)
+ 
+ 
 
 if __name__ == '__main__':
     main()
-
